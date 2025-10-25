5b3cecb20e939b146b4f013af815051be801efcc
Alperen012 <125201092+Alperen012@users.noreply.github.com>
2025-10-25 11:36:22 +0300

    Time: 19 ms (15.10%) | Memory: 18 MB (32.96%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,28 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_map = {}
+
+        for i in s:
+            if(s_map.get(i) != None):
+                s_map[i] = s_map[i] + 1
+            else:
+                s_map[i] = 1
+                
+        t_map = {}
+
+        for i in t:
+            if(t_map.get(i) != None):
+                t_map[i] = t_map[i] + 1
+            else:
+                t_map[i] = 1
+        
+        if(s_map == t_map):
+            return True
+        return False
+        
+        
+
+
+        
+
+        
